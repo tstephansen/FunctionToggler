aaa6fbf54ec04978138955de2761fb5daba7b997
Tim Stephansen <Tim.Stephansen@newmindtech.com>
2026-03-25 12:10:18 -0400

    Initial Commit

---
+++ FunctionToggler/FunctionTogglerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunctionTogglerApp.swift
+//  FunctionToggler
+//
+//  Created by Tim Stephansen on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct FunctionTogglerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
